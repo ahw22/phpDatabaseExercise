a13ef0627cf65937281494e454168398f202e4c5
ahw22 <andreas.zincke@gmx.at>
2024-11-14 11:10:39 +0100

    add user registration

--- src/register.php
+++ src/register.php
@@ -1,11 +1,52 @@
 <?php
+
+$serverName = "localhost";
+$username = "root";
+$dbName = "users";
+
+//create db connection
+$conn = mysqli_connect('localhost', 'root', '', 'users');
+//check connection
+if (!$conn) {
+    die("Connection failed: ");
+}
+echo "Connected sucessfully\n";
+/*
+$sql = "CREATE DATABASE users";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully\n";
+} else {
+    echo "Error creating database: " . $conn->error . "\n";
+}
+
+//create table
+$sql = "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table users creates successfully \n";
+} else {
+    echo "Error creating table: " . $conn->error . "\n";
+}
+*/
+
+//get data from form
 $name = htmlspecialchars($_POST["uName"]);
 $email = htmlspecialchars($_POST["email"]);
 $password = htmlspecialchars($_POST["password"]);
 
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
-
-
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 
 
@@ -35,7 +76,6 @@ $password = htmlspecialchars($_POST["password"]);
 </head>
 
 <body>
-    Welcome <?php echo $_POST["uName"]; ?>
 
 
 
